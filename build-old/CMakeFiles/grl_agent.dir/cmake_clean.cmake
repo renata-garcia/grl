388619710f7a0597668cd236119e9544530b4f47
Renata Garcia <renata.garcia.eng@gmail.com>
2018-09-24 20:02:23 -0300

    build-old was removed

--- build-old/CMakeFiles/grl_agent.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/grl_agent"
-  "grl_agent.mexa64"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/grl_agent.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build-old/CMakeFiles/grl_env.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/grl_env"
-  "grl_env.mexa64"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/grl_env.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
