a935de9f65d3de3b8b4205477e053c5c3c9b60aa
Renata Garcia <renata.garcia.eng@gmail.com>
2019-01-22 22:15:35 -0200

    yamls

--- bin/script.sh
+++ bin/script.sh
@@ -86,8 +86,6 @@ python3 ../addons/tensorflow/share/pendulum_ddpg.py
 ./grld ../cfg/cart_pole/mpol_dpg_26_density_based_voting_mov.yaml
 ./grld ../cfg/cart_pole/mpol_dpg_26_density_based_voting_mov_two_steps.yaml
 
-'
-
 ./grld ../cfg/pendulum/mpol_dpg_13_density_based_historic.yaml
 ./grld ../cfg/pendulum/mpol_dpg_13_density_based_historic_a001.yaml
 ./grld ../cfg/pendulum/mpol_dpg_20_density_based_historic.yaml
@@ -95,6 +93,13 @@ python3 ../addons/tensorflow/share/pendulum_ddpg.py
 ./grld ../cfg/pendulum/mpol_dpg_26_density_based_historic.yaml
 ./grld ../cfg/pendulum/mpol_dpg_26_density_based_historic_a001.yaml
 
+'
+
 ./grld ../cfg/pendulum/mpol_dpg_13_density_based_historic_dens.yaml
 ./grld ../cfg/pendulum/mpol_dpg_20_density_based_historic_dens.yaml
 ./grld ../cfg/pendulum/mpol_dpg_26_density_based_historic_dens.yaml
+
+./grld ../cfg/pendulum/mpol_dpg_13_density_based_historic_a10.yaml
+./grld ../cfg/pendulum/mpol_dpg_20_density_based_historic_a10.yaml
+./grld ../cfg/pendulum/mpol_dpg_26_density_based_historic_a10.yaml
+
