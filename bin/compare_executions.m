92a7637cc24aa5df804576131225c3265bb6cfa5
Renata NOTE <renata.garcia.eng@gmail.com>
2019-02-02 19:04:29 -0200

    scripts

--- bin/compare_executions.m
+++ bin/compare_executions.m
@@ -1,5 +1,5 @@
-function compare_executions(n, folder, title_fig, varargin)
-    for i = 1:(nargin -3)
+function compare_executions(n, folder, nstd, title_fig, varargin)
+    for i = 1:(nargin -4)
         name = varargin{i};
         file = folder + name;
         if i == 1
@@ -11,18 +11,31 @@ function compare_executions(n, folder, title_fig, varargin)
 
     figure('units','normalized','outerposition',[0 0 1 1]);
     leg = [];
+    color = ['r', 'g', 'b', 'y', 'm', 'c', 'w', 'k'];
+    num_alg = length(array);
+    h = zeros(1, num_alg);
     for j=1:length(array)
-        mean = load_mean(array(j,2), n);
-        maximum = max(mean);
-        x = 1:length(mean);
-        plot(x, mean);
+        [mean_d, std_d] = load_mean(array(j,2), n);
+        errstd_d = std_d/sqrt(n);
+        maximum = max(mean_d);
+        x = 1:length(mean_d);
+        n_color = color(rem(j, length(color)));
+        h(j) = plot(x, mean_d, 'Color', n_color, 'LineWidth', 2, 'LineStyle','-', 'DisplayName', strcat(array(j,1), ':... ', num2str(maximum)));
         leg = [leg, strcat(array(j,1), ':... ', num2str(maximum))];
         hold on;
+        plot(x, [mean_d - nstd*std_d; mean_d + nstd*std_d], 'Color', n_color, 'LineWidth', 0.3, 'LineStyle','-.');
+        leg = [leg, ['-', num2str(nstd), '*std']];
+        leg = [leg, [num2str(nstd), '*std']];
+        hold on;
+        plot(x, [mean_d - errstd_d; mean_d + errstd_d], 'Color', n_color, 'LineWidth', 0.5, 'LineStyle',':', 'HandleVisibility','off');
+        leg = [leg, ['-', 'err_std']];
+        leg = [leg, ['err_std']];
+        hold on;
     end
-
+    
     grid;
     title(title_fig);
     xlabel('step');
     ylabel('reward');
-    legend(leg,'Location','SouthEast');
+    legend(h,'Location','SouthEast');
 end
--- bin/load_mean.m
+++ bin/load_mean.m
@@ -1,7 +1,13 @@
-function mean = load_mean(file, n)
+function [mean_d, std_d] = load_mean(file, n)
+    at = load(file + "-0.txt");
+    s = size(at);
+    data = zeros(n,s(1));
+    
     sum = load(file + "-0.txt");
-    for i=1:(n-1)
-        sum = sum + load(file + "-" + i + ".txt");
+    for i=1:n
+        tmp = load(file + "-" + (i-1) + ".txt");
+        data(i,:) = tmp(:,3);
     end
-    mean = sum(:,3)/n;
+    mean_d = mean(data);
+    std_d = std(data);
 end
