c5f32f3561fb32f37ded37c3946a3fbe13393b85
Renata Garcia <renata.garcia.eng@gmail.com>
2019-02-14 00:35:36 -0200

    matlab script

--- bin/compare_executions.m
+++ bin/compare_executions.m
@@ -1,4 +1,5 @@
 function compare_executions(n, folder, nstd, title_fig, varargin)
+    size_windows = [0 0 0.40 0.40];
     for i = 1:(nargin -4)
         name = varargin{i};
         file = folder + name;
@@ -9,39 +10,39 @@ function compare_executions(n, folder, nstd, title_fig, varargin)
         end
     end
 
-    figure('units','normalized','outerposition',[0 0 1 1]);
-    leg = [];
-    color = ['y', 'm', 'b', 'r', 'k', 'g', 'c'];
-    num_alg = length(array);
-    h = zeros(1, num_alg);
-    for j=1:length(array)
-        [mean_d, std_d] = load_mean(array(j,2), n);
-        errstd_d = std_d/sqrt(n);
-        maximum = max(mean_d);
-        x = 1:length(mean_d);
-        n_color = color(1 + rem(j, length(color)));
-        h(j) = plot(x, mean_d, 'Color', n_color, 'LineWidth', 1.3, 'LineStyle','-', 'DisplayName', strcat(array(j,1), ':... ', num2str(maximum)));
-        leg = [leg, strcat(array(j,1), ':... ', num2str(maximum))];
-        hold on;
-        plot(x, [mean_d - nstd*std_d; mean_d + nstd*std_d], 'Color', n_color, 'LineWidth', 0.3, 'LineStyle','-.');
-        leg = [leg, ['-', num2str(nstd), '*std']];
-        leg = [leg, [num2str(nstd), '*std']];
-        hold on;
-        plot(x, [mean_d - errstd_d; mean_d + errstd_d], 'Color', n_color, 'LineWidth', 0.5, 'LineStyle',':', 'HandleVisibility','off');
-        leg = [leg, ['-', 'err_std']];
-        leg = [leg, ['err_std']];
-        hold on;
-    end
-    
-    grid;
-    title(title_fig);
-    xlabel('step');
-    ylabel('reward');
-    legend(h,'Location','SouthEast');
+%     figure('units','normalized','outerposition',size_windows);
+%     leg = [];
+%     color = ['y', 'm', 'b', 'r', 'k', 'g', 'c'];
+%     num_alg = length(array);
+%     h = zeros(1, num_alg);
+%     for j=1:length(array)
+%         [mean_d, std_d] = load_mean(array(j,2), n);
+%         errstd_d = std_d/sqrt(n);
+%         maximum = max(mean_d);
+%         x = 1:length(mean_d);
+%         n_color = color(1 + rem(j, length(color)));
+%         h(j) = plot(x, mean_d, 'Color', n_color, 'LineWidth', 1.3, 'LineStyle','-', 'DisplayName', strcat(array(j,1), ':... ', num2str(maximum)));
+%         leg = [leg, strcat(array(j,1), ':... ', num2str(maximum))];
+%         hold on;
+%         plot(x, [mean_d - nstd*std_d; mean_d + nstd*std_d], 'Color', n_color, 'LineWidth', 0.3, 'LineStyle','-.');
+%         leg = [leg, ['-', num2str(nstd), '*std']];
+%         leg = [leg, [num2str(nstd), '*std']];
+%         hold on;
+%         plot(x, [mean_d - errstd_d; mean_d + errstd_d], 'Color', n_color, 'LineWidth', 0.5, 'LineStyle',':', 'HandleVisibility','off');
+%         leg = [leg, ['-', 'err_std']];
+%         leg = [leg, ['err_std']];
+%         hold on;
+%     end
+%     
+%     grid;
+%     title(title_fig);
+%     xlabel('step');
+%     ylabel('reward');
+%     legend(h,'Location','SouthEast');
 
 %   ------------------------------------------------------
     
-    figure('units','normalized','outerposition',[0 0 1 1]);
+    figure('units','normalized','outerposition',size_windows);
     leg = [];
     color = ['y', 'm', 'b', 'r', 'k', 'g', 'c'];
     num_alg = length(array);
@@ -66,37 +67,37 @@ function compare_executions(n, folder, nstd, title_fig, varargin)
 
 %   ------------------------------------------------------
 
-    for j=1:length(array)
-        figure();
-        [mean_d, std_d] = load_mean(array(j,2), n);
-        errstd_d = std_d/sqrt(n);
-        maximum = max(mean_d);
-        x = 1:length(mean_d);
-        n_color = color(1 + rem(j, length(color)));
-        errorbaralpha(mean_d, errstd_d, 'color', n_color, 'linestyle', '--');
-        leg = [strcat('err: ', array(j,1), ':... ', num2str(maximum))];
-        grid;
-        title(title_fig);
-        xlabel('step');
-        ylabel('reward');
-        legend(leg,'Location','SouthEast');
-    end 
+%     for j=1:length(array)
+%         figure('units','normalized','outerposition',size_windows);
+%         [mean_d, std_d] = load_mean(array(j,2), n);
+%         errstd_d = std_d/sqrt(n);
+%         maximum = max(mean_d);
+%         x = 1:length(mean_d);
+%         n_color = color(1 + rem(j, length(color)));
+%         errorbaralpha(mean_d, errstd_d, 'color', n_color, 'linestyle', '--');
+%         leg = [strcat('err: ', array(j,1), ':... ', num2str(maximum))];
+%         grid;
+%         title(title_fig);
+%         xlabel('step');
+%         ylabel('reward');
+%         legend(leg,'Location','SouthEast');
+%     end 
 
 %   ------------------------------------------------------
 
-    for j=1:length(array)
-        figure();
-        [mean_d, std_d] = load_mean(array(j,2), n);
-        errstd_d = std_d/sqrt(n);
-        maximum = max(mean_d);
-        x = 1:length(mean_d);
-        n_color = color(1 + rem(j, length(color)));
-        errorbaralpha(mean_d, std_d, 'color', n_color, 'linestyle', '--');
-        leg = [strcat('std: ', array(j,1), ':... ', num2str(maximum))];
-        grid;
-        title(title_fig);
-        xlabel('step');
-        ylabel('reward');
-        legend(leg,'Location','SouthEast');
-    end 
+%     for j=1:length(array)
+%         figure('units','normalized','outerposition',size_windows);
+%         [mean_d, std_d] = load_mean(array(j,2), n);
+%         errstd_d = std_d/sqrt(n);
+%         maximum = max(mean_d);
+%         x = 1:length(mean_d);
+%         n_color = color(1 + rem(j, length(color)));
+%         errorbaralpha(mean_d, std_d, 'color', n_color, 'linestyle', '--');
+%         leg = [strcat('std: ', array(j,1), ':... ', num2str(maximum))];
+%         grid;
+%         title(title_fig);
+%         xlabel('step');
+%         ylabel('reward');
+%         legend(leg,'Location','SouthEast');
+%     end 
 end
