4fd2396f8b4aec7031263fc42ad69a3725a9b8b4
Renata Lenovo <renata.garcia.eng@gmail.com>
2019-04-12 15:00:01 -0300

    yamls

--- bin/comparing_cart_pole.m
+++ bin/comparing_cart_pole.m
@@ -11,7 +11,7 @@ addpath("~/Dropbox/phd_grl_results/matlab");
 %reward scale 0.1
 %replay 128, batch_size 52
 
-f = ["cart_pole_replay_ddpg_tensorflow_*.txt",...
+f = ["cartpole_mpol_1_replay_ddpg_tensorflow_replay_steps_128_batch_size_64_reward_010_*.txt",...
     "cart_pole_new/cart_pole_replay_ddpg_tensorflow_geometric_replay_steps128_batch_size128*.txt" , ...
     "cart_pole_new/cart_pole_replay_ddpg_tensorflow_geometric_replay_steps64_batch_size64*.txt"];
 
@@ -26,7 +26,7 @@ num_alg = length(f);
 h = zeros(1, num_alg);
 for j=1:size(f,2)
     fd = "/home/renatargo/Dropbox/phd_grl_results/phd_grl_mpol_results/" + f(j);
-    data = readseries(fd, 3, 1, 33);
+    data = readseries(fd, 3, 2, 33);
     [t, mean_d, ~, std_d] = avgseries(data);
     %errstd_d = std_d/sqrt(n);
     maximum = max(mean_d);
