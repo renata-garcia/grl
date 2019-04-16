aa2a5b2dd346d2c287c43abe80d81c5ad2b2749a
Renata Garcia <renata.garcia.eng@gmail.com>
2019-04-16 00:13:37 -0300

    yamls

--- cfg/cart_pole/spec_cart_pole_dpg_16_2.sh
+++ cfg/cart_pole/spec_cart_pole_dpg_16_2.sh
@@ -1,3 +1,3 @@
 sh ../bin/clean_build.sh
-cp ~/projects/grl/cfg/pendulum/env_cart_pole.yaml ~/projects/grl/build
-cp ~/projects/grl/cfg/cart_pole/agent_mpol_dpg_16_2.yaml ~/projects/grl/build
+cp ~/projects/grl/cfg/cart_pole/env_cart_pole.yaml ~/projects/grl/build
+cp ~/projects/grl/cfg/cart_pole/agent_mpol_ddpg_16_2.yaml ~/projects/grl/build
