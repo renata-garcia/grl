3c6f5dba8da7bd3d352019829b306d791b99ecf0
Renata Lenovo <renata.garcia.eng@gmail.com>
2019-08-14 02:31:35 -0300

    sript matlab

--- bin/comp_taking_perf.m
+++ bin/comp_taking_perf.m
@@ -6,18 +6,21 @@ close all;
 steps_counted = 10;
 ie=1;
 ia=3;
-exc=0;
 withLoad = 0;
+withlimiar = 1;
+onlybad4pend=0;
 if (ie == 1)
     env = "pendulum"; env_abr = "pd";
 elseif (ie == 2)
     env = "cart_pole"; env_abr = "cp";
+elseif (ie == 3)
+    env = "cart_double_pole"; env_abr = "cdp";
 end
 algs = ["ac_tc", "dpg", "ddpg"];
 alg = algs(ia);
-tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc);
-title_leg = strcat("env= ", env, " alg= ", algs(ia));
-test_printL_1env_tbl_all_std(title_leg, tbl_meanstd_all, withLoad)
+[tbl_meanstd_all, percentual] = generate_tbl(env, env_abr, alg, withLoad, onlybad4pend, withlimiar);
+title_leg = strcat("env= ", env, " alg= ", algs(ia), " limi-3500");
+test_printL_1env_tbl_all_std_perc(title_leg, tbl_meanstd_all, withLoad, percentual)
 %%%%%% [perf_good, perf_mid, perf_bad, perf_stdgood, perf_stdmid, perf_stdbad, bpd, bstdpd, bcp, bstdcp, bcdp, bstdcdp] = generate_tables(steps_counted);
 
 function [perf_good, perf_mid, perf_bad, perf_stdgood, perf_stdmid, perf_stdbad, bpd, bstdpd, bcp, bstdcp, bcdp, bstdcdp] = generate_tables(steps_counted)
@@ -324,7 +327,7 @@ function fprinttex(v, s, up, down)
     end
 end
 
-function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
+function [tbl_meanstd_all, percentual] = generate_tbl(env, env_abr, alg, withLoad, exc, withlimiar)
     steps_counted = 10;
     printing = 1;
     folder = "~/Dropbox/phd_grl_results/phd_grl_mpol_results/";
@@ -334,12 +337,12 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
     if (exc)
         group = ["bad"];
     end
-    
+
     load = [""];
     if(withLoad)
         load = ["", "_load"];
     end
-    
+
     n_group = length(group);
     n_load = length(load);
     ng = 2*n_group;
@@ -347,7 +350,9 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
 
     % %
     n_env = 1;
-    tbl_meanstd_all = zeros(24, n_env*ng*n_load);
+    n_vert = 24;
+    tbl_meanstd_all = zeros(n_vert, n_env*ng*n_load);
+    percentual = zeros(n_vert, n_group);
 
     if (contains(env,"cart"))
         steps_per_second = 20;
@@ -385,9 +390,9 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
                             group(ig) + load(il) + "_*_none_density_0.5_density_a0.01*txt"];
             i_load = (ng*n_load) + ng*(il-1) + (2*ig)-1; %(ng*n_load)*(ie-1) + ng*(il-1) + (2*ig)-1
             j_load = (ng*n_load) + ng*(il-1) + (2*ig); %(ng*n_load)*(ie-1) + ng*(il-1) + (2*ig)
-            i = (ng*n_load) + nl*(ig-1) + 2*il -1;
-            j = (ng*n_load) + nl*(ig-1) + 2*il;
-            [tbl_meanstd_all(:, i:j)] = test_take_mean_mpol(folder, env, env_abr, load(il), alg, runs_generic, printing, steps_per_second, steps_counted);
+            i = nl*(ig-1) + 2*il -1; %(ng*n_load)*(ie-1) + 
+            j = nl*(ig-1) + 2*il;%(ng*n_load)*(ie-1) + 
+            [tbl_meanstd_all(:, i:j), percentual(:,ig)] = test_take_mean_mpol(folder, env, env_abr, load(il), alg, runs_generic, printing, steps_per_second, steps_counted, withlimiar);
         end
 	end
 
@@ -400,14 +405,14 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
                                           type + "_*_none_density_linear_order_0.1_best_a0.01_*.txt",...
                                           type + "_*_none_density_linear_order_0.5_data_center_a0.01_*.txt",...
                                           type + "_*_none_density_linear_order_0.5_density_a0.01_*.txt"];
-            tbl_meanstd_specific_good = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_good, printing, steps_per_second, steps_counted);
+            [tbl_meanstd_specific_good, percentual_spec_good] = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_good, printing, steps_per_second, steps_counted, withlimiar);
 
             type = "mid";
             runs_specific_mid = [         type + "_*_mean_euclidian_distance_0.5_density_a0.01_*.txt",...
                                   "r05" + type + "_*_mean_euclidian_distance_0.5_density_a0.01_*.txt",...
                                   "r25" + type + "_*_mean_euclidian_distance_0.25_density_a0.01_*.txt",...
                                   "r25" + type + "_*_mean_euclidian_distance_0.5_density_a0.01_*.txt"];
-            tbl_meanstd_specific_mid = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_mid, printing, steps_per_second, steps_counted);
+            [tbl_meanstd_specific_mid, percentual_spec_mid] = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_mid, printing, steps_per_second, steps_counted, withlimiar);
 
             type = "bad";
             runs_specific_bad = [         type + "_*_none_data_center_linear_order_0.25_data_center_a0.01_*txt",...
@@ -427,7 +432,7 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
                                   "r25" + type + "_*_mean_euclidian_distance_0.25_density_a0.01_*.txt",...
                                   "r25" + type + "_*_mean_euclidian_distance_0.5_density_a0.01_*.txt",...
                           "r50" + type + "_*_mean_euclidian_distance_0.5_density_a0.01_*.txt"];
-        tbl_meanstd_specific_bad = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_bad, printing, steps_per_second, steps_counted);
+        [tbl_meanstd_specific_bad, percentual_spec_bad] = test_take_mean_mpol(folder, env, env_abr, "", alg, runs_specific_bad, printing, steps_per_second, steps_counted, withlimiar);
         
         disp(tbl_meanstd_specific_good);
         disp(tbl_meanstd_specific_mid);
@@ -440,7 +445,7 @@ function tbl_meanstd_all = generate_tbl(env, env_abr, alg, withLoad, exc)
 %disp(tbl_meanstd_all);
 end
 
-function means_std = test_take_mean_mpol(folder, env, env_abr, load, alg, runs, printing, steps_per_second, steps_counted)
+function [means_std, percentual] = test_take_mean_mpol(folder, env, env_abr, load, alg, runs, printing, steps_per_second, steps_counted, withLimiar)
     subfolder = "";
     preffix = "";
     
@@ -461,19 +466,35 @@ function means_std = test_take_mean_mpol(folder, env, env_abr, load, alg, runs, 
     if (printing)
         disp(fd);
     end
-    [t, mean_d, ~, std_d] = avgseries(readseries(fd, 3, 2, steps_per_second));
+    [t, mean_d, ~, std_e] = avgseries(readseries(fd, 3, 2, steps_per_second));
 
     means_std = zeros(length(array_runs), 2);
+    percentual = zeros(length(array_runs), 1);
     for j=1:length(array_runs)
         fd = folder + array_runs(j);
         if (printing)
             disp(fd);
         end
-        [t, mean_d, ~, std_d] = avgseries(readseries(fd, 3, 2, steps_per_second));
+        data_no_limiar = readseries(fd, 3, 2, steps_per_second);
+        data = {};
+        k = 0;
+%         inc = 100/length(data_no_limiar);
+        for i=1:length(data_no_limiar)
+            tmp = data_no_limiar{i};
+            if (mean(tmp((end-5):end,2)) > -3500)
+                k = k + 1;
+                data{k} = data_no_limiar{i};
+                percentual(j) = percentual(j) + 1;
+            end
+        end
+        if (length(data) > 0)
+            [t, mean_d, ~, std_e] = avgseries(data);
+        else
+            [t, mean_d, ~, std_e] = avgseries(data_no_limiar);
+        end
         means_std(j, 1) = mean(mean_d(length(mean_d)-steps_counted+1:length(mean_d)));
-        means_std(j, 2) = mean(std_d(length(std_d)-steps_counted+1:length(std_d)));
+        means_std(j, 2) = mean(std_e(length(std_e)-steps_counted+1:length(std_e)));
     end
-    
 end
 
 function test_printL_1env_tbl_all_std(caption, tbl, withLoad)
@@ -550,6 +571,96 @@ function test_printL_1env_tbl_all_std(caption, tbl, withLoad)
                 fprintf("\\textbf{%.0f},\\textbf{%.0f}", tbl(j, i:i+1));
             else
                 fprintf("%.0f,%.0f", tbl(j, i:i+1));
+            end
+        end
+        if (j == size(tbl, 1))
+            fprintf(" \\\\ \\hline \n");
+        else
+            fprintf(" \\\\ \\cline{2-8} \n");
+        end
+    end
+    fprintf("  \\end{tabular}\n");
+    fprintf("\\end{table*}\n");
+end
+
+function test_printL_1env_tbl_all_std_perc(caption, tbl, withLoad, percentual)
+    sz_base = 4;
+    ind_max = 1;
+    jmp_grp = 2;
+    if(withLoad)
+        ind_max = 3;
+        jmp_grp = 4;
+    end
+    bests =  zeros(1, 3);
+    j = 1;
+    for i=ind_max:jmp_grp:size(tbl,2)
+    	max_base = max(tbl(1:sz_base, i));
+        ind_max_base = tbl(1:sz_base, i) == max_base;
+        iline = (1:sz_base)*ind_max_base;
+        max_basestd = max_base - tbl(iline,i+1);
+        bests(j) = max_basestd;
+        j=j+1;
+    end
+    disp(bests);
+    strategies = ["DC", "D", "M", "RND", "DC\_MA\_B", "DC\_MA\_25\_DC", "DC\_MA\_50\_DC",...
+                  "DC\_MA\_75\_DC", "DC\_MA\_50\_D", "D\_MA\_50\_D", "D\_MA\_50\_DC", "D\_MA\_B",...
+                  "DC\_ED\_MA\_25\_DC", "DC\_EC\_MA\_50\_DC", "DC\_ED\_MA\_75\_DC", "DC\_ED\_MA\_B",...
+                  "D\_ED\_MA\_B", "DC\_MA\_B", "M\_ED\_MA\_B", "M\_ED\_MA\_25\_D", "M\_ED\_MA\_50\_D",...
+                  "M\_ED\_MA\_75\_D", "M\_ED\_MA\_50\_DC", "DC\_MA\_50\_D"];
+              
+	fprintf("%% Please add the following required packages to your document preamble:\n");
+    fprintf("%% \\usepackage{multirow}\n");
+    fprintf("  \\begin{table*}[]\n");
+    fprintf("  \\centering\n");
+    fprintf("  \\footnotesize\n");
+    fprintf("  \\caption{%s}\n", caption);
+    fprintf("  \\label{tab_performance_all}\n");
+    fprintf("  \\begin{tabular}{l|c|D{,}{\\pm}{-1}|D{,}{\\pm}{-1}|D{,}{\\pm}{-1}|D{,}{\\pm}{-1}|D{,}{\\pm}{-1}|D{,}{\\pm}{4.4}|}\n");
+    fprintf("    \\cline{2-8}\n");
+    fprintf("     & \\multirow{2}{*}{strategy} & \\multicolumn{2}{c|}{good} & \\multicolumn{2}{c|}{mid} & \\multicolumn{2}{c|}{bad} \\\\ \\cline{3-8} \n");
+    fprintf("     &  & \\multicolumn{1}{|c|}{learning} & \\multicolumn{1}{|c|}{learned} & \\multicolumn{1}{|c|}{learning} & \\multicolumn{1}{|c|}{learned} & \\multicolumn{1}{|c|}{learning} & \\multicolumn{1}{|c|}{learned} \\\\ \\hline\n");
+    for j=1:sz_base
+        if (j == 1)
+            fprintf("    \\multicolumn{1}{|l|}{\\multirow{%d}{*}", sz_base);
+            fprintf("{\\STAB{\\rotatebox[origin=c]{90}{BASE}}}} & \\footnotesize{%s}", strategies(j));
+        else
+            fprintf("    \\multicolumn{1}{|l|}{} & \\footnotesize{%s}", strategies(j));
+        end
+        k = 1;
+        ii = 0;
+        for i=1:2:size(tbl,2)
+            ii = ii + 1;
+            k = 1 + floor(i/jmp_grp);
+            fprintf(" & ");
+            if ((tbl(j, i)+tbl(j, i+1)) >= bests(k))
+                fprintf("\\textbf{%.0f},\\textbf{%.0f} (%d)", tbl(j, i:i+1), percentual(j,ii));
+            else
+                fprintf("%.0f,%.0f (%d)", tbl(j, i:i+1), percentual(j,ii));
+            end
+        end
+        if(j==sz_base)
+            fprintf(" \\\\ \\hline \n");
+        else
+           fprintf(" \\\\ \\cline{2-8} \n");
+        end
+        
+    end
+    for j=(sz_base + 1):size(tbl, 1)
+        if (j == (sz_base + 1))
+            fprintf("    \\multicolumn{1}{|l|}{\\multirow{%d}{*}", (size(tbl, 1)-sz_base));
+            fprintf("{\\STAB{\\rotatebox[origin=c]{90}{NEW}}}} & \\footnotesize{%s}", strategies(j));
+        else
+            fprintf("    \\multicolumn{1}{|l|}{} & \\footnotesize{%s}", strategies(j));
+        end
+        ii = 0;
+        for i=1:2:size(tbl,2)
+            ii = ii + 1;
+            k = 1 + floor(i/jmp_grp);
+            fprintf(" & ");
+            if ((tbl(j, i)-tbl(j, i+1)) >= bests(k))
+                fprintf("\\textbf{%.0f},\\textbf{%.0f} (%d)", tbl(j, i:i+1), percentual(j,ii));
+            else
+                fprintf("%.0f,%.0f (%d)", tbl(j, i:i+1), percentual(j,ii));
             end
         end
         if (j == size(tbl, 1))
