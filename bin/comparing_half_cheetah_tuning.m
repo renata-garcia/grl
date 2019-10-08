03255c5c7365297912f1ff60f1b935f6789e6e0e
Renata Garcia <renata.garcia.eng@gmail.com>
2019-10-07 23:20:58 -0300

    script

--- bin/comparing_half_cheetah_tuning.m
+++ bin/comparing_half_cheetah_tuning.m
@@ -5,7 +5,7 @@ close all;
 n = 10;
 folder = "~/Dropbox/phd_grl_results/phd_grl_mpol_results/half_cheetah_tuning/";
 addpath("~/Dropbox/phd_grl_results/matlab");
-steps_per_second = 1;
+steps_per_second = 100;
 %%
 array_runs = [
 "half_cheetah_hc_ddpg_replay_steps128_batch_size128_interval400_gamma0.4_reward_scale0.01_sigma[1]*txt",...
@@ -209,3 +209,30 @@ title_fig = "FINDING BEST SINGLE HC";
 
 ploting_executions(folder, title_fig, steps_per_second, array_runs);
 
+%%
+array_runs = [
+"half_cheetah_hc_ddpg_replay_steps256_batch_size128_interval100_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps256_batch_size128_interval10_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps256_batch_size64_interval100_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps256_batch_size64_interval10_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size128_interval100_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size128_interval10_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size64_interval100_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size64_interval10_gamma0.99_reward_scale0.01_sigma[1]*txt"];
+
+title_fig = "FINDING BEST SINGLE HC";
+
+ploting_executions(folder, title_fig, steps_per_second, array_runs);
+
+
+%%
+array_runs = [
+"half_cheetah_hc_ddpg_replay_steps256_batch_size128_interval100_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size128_interval10000_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps512_batch_size64_interval10000_gamma0.99_reward_scale0.01_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps256_batch_size64_interval1000_gamma0.99_reward_scale0.1_sigma[1]*txt",...
+"half_cheetah_hc_ddpg_replay_steps256_batch_size128_interval1000_gamma0.99_reward_scale0.1_sigma[1]*txt"];
+
+title_fig = "FINDING BEST SINGLE HC";
+
+ploting_executions(folder, title_fig, steps_per_second, array_runs);
