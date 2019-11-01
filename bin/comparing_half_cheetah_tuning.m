c442cd9bc14ebf9202c1e8b6856fac6bc750d980
Renata Lenovo <renata.garcia.eng@gmail.com>
2019-11-01 18:46:44 -0300

    taking mean and stderr of each run in half cheetah

--- bin/comparing_half_cheetah_tuning.m
+++ bin/comparing_half_cheetah_tuning.m
@@ -259,3 +259,30 @@ array_runs = [
 title_fig = "FINDING BEST SINGLE HC";
 
 ploting_executions(folder, title_fig, steps_per_second, array_runs);
+
+%%
+steps_counted = 10;
+printing = 1;
+
+folder_old = folder;
+folder = "~/Dropbox/phd_grl_results/phd_grl_mpol_results/half_cheetah_mpols_yamls_results/";
+
+array_runs = [
+    "half_cheetah_tau_replay_ddpg_tensorflow_sincos16good_j*_none_none_1.0_data_center_a1.0_*.txt",...
+    "half_cheetah_tau_replay_ddpg_tensorflow_sincos16good_j*_none_none_1.0_density_a1.0_*.txt",...
+    "half_cheetah_tau_replay_ddpg_tensorflow_sincos16good_j*_data_center_euclidian_distance_0.25_data_center_a0.01_*.txt",...
+    "half_cheetah_tau_replay_ddpg_tensorflow_sincos16good_j*_none_data_center_linear_order_0.25_data_center_a0.01_*.txt",...
+    "half_cheetah_tau_replay_ddpg_tensorflow_sincos16good_j*_mean_euclidian_distance_0.25_density_a0.01_*txt"];
+
+   
+for j=1:length(array_runs)
+    fd = folder + array_runs(j);
+    if (printing)
+        disp(fd);
+    end
+    [t, mean_d, ~, std_e] = avgseries(readseries(fd, 3, 2, steps_per_second));
+    disp(mean(mean_d(length(mean_d)-steps_counted+1:length(mean_d))));
+    disp(mean(std_e(length(std_e)-steps_counted+1:length(std_e))));
+end
+
+folder = folder_old;
